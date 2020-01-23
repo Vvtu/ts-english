34b091c708b1b9119dac8c4682adc17642f62f6f
Vvtu <vvtu@icloud.com>
2020-01-23 13:57:48 +0300

    Updates

---
+++ precache-manifest.d55c62c6f454fac3df83381640d26522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b606b6dfd4cbbfbfbc3a80183367b6e9",
+    "url": "/ts-english/index.html"
+  },
+  {
+    "revision": "5bdf7a72735632af2c61",
+    "url": "/ts-english/static/css/main.b9bcdeba.chunk.css"
+  },
+  {
+    "revision": "7884b10d0838833e1203",
+    "url": "/ts-english/static/js/2.dca998a6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ts-english/static/js/2.dca998a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bdf7a72735632af2c61",
+    "url": "/ts-english/static/js/main.ae4f0c95.chunk.js"
+  },
+  {
+    "revision": "09fa1a7ffd9a3b363a34",
+    "url": "/ts-english/static/js/runtime-main.b3511124.js"
+  }
+]);
