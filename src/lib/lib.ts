d73d8dcfea6deed750593039ed81c18f69333a97
Vvtu <vvtu@icloud.com>
2020-02-02 11:22:19 +0300

    fix new words

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,65 +1,66 @@
-import { dictType, dictionariesType } from '../Types';
-import { NUMBER_OF_PAIRS_FROM_OLD_DICTIONARY } from '../Constants/constants';
+import { dictType, dictionariesType } from "../Types";
+import { NUMBER_OF_PAIRS_FROM_OLD_DICTIONARY } from "../Constants/constants";
 
 export const getRandomInt = (min: number, max: number) => {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	//  The  min is inclusive  and the max is exclusive
-	return Math.floor(Math.random() * (max - min)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  //  The  min is inclusive  and the max is exclusive
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
 const reshuffle = (dic: any[]) =>
-	dic
-		.map((item) => [ Math.random(), item ])
-		.sort((a, b) => a[0] - b[0])
-		.map((elem) => elem[1]);
+  dic
+    .map(item => [Math.random(), item])
+    .sort((a, b) => a[0] - b[0])
+    .map(elem => elem[1]);
 
 export const filterDeletedOff = (arr: dictType[]) => {
-	const result = (arr || []).filter((elem) => {
-		const russian = Object.keys(elem || {})[0];
-		if (russian) {
-			const item = localStorage.getItem(russian);
-			return item !== 'd';
-		} else {
-			return false;
-		}
-	});
-	return result;
+  const result = (arr || []).filter(elem => {
+    const russian = Object.keys(elem || {})[0];
+    if (russian) {
+      const item = localStorage.getItem(russian);
+      return item !== "d";
+    } else {
+      return false;
+    }
+  });
+  return result;
 };
 
-const DICT = 'DICT';
-const DICT_NUMBER = '2';
+const DICT = "DICT";
+const DICT_NUMBER = "2";
 
 export const getDictionaryWithMix = (
-	dictionaries: dictionariesType,
+  dictionaries: dictionariesType
 ) => {
-	const whichDict = localStorage.getItem(DICT);
-	let d: dictType[];
-	let oldD: dictType[];
-	if (whichDict === DICT_NUMBER) {
-		d = dictionaries.dictionary2;
-		oldD = dictionaries.oldDictionary2.concat(
-			dictionaries.dictionary20,
-		);
-	} else {
-		d = dictionaries.dictionary1;
-		oldD = dictionaries.oldDictionary1.concat(
-			dictionaries.dictionary10,
-		);
-	}
-	d = reshuffle(filterDeletedOff(d));
-	oldD = reshuffle(filterDeletedOff(oldD)).slice(
-		0,
-		NUMBER_OF_PAIRS_FROM_OLD_DICTIONARY,
-	);
+  const whichDict = localStorage.getItem(DICT);
+  let d: dictType[];
+  let oldD: dictType[];
+  if (whichDict === DICT_NUMBER) {
+    d = dictionaries.dictionary2;
+    oldD = dictionaries.oldDictionary2.concat(
+      dictionaries.dictionary20
+    );
+  } else {
+    d = dictionaries.dictionary1;
+    oldD = dictionaries.oldDictionary1.concat(
+      dictionaries.dictionary10
+    );
+  }
+  d = d.filter(({ rus, eng }) => !(rus === "" && eng === ""));
+  d = reshuffle(filterDeletedOff(d));
+  oldD = reshuffle(filterDeletedOff(oldD)).slice(
+    0,
+    NUMBER_OF_PAIRS_FROM_OLD_DICTIONARY
+  );
 
-	return oldD.concat(d);
+  return oldD.concat(d);
 };
 
 export const handleDictClicked = (number: number) => {
-	if (number === 2) {
-		localStorage.setItem(DICT, DICT_NUMBER);
-	} else {
-		localStorage.setItem(DICT, '1');
-	}
+  if (number === 2) {
+    localStorage.setItem(DICT, DICT_NUMBER);
+  } else {
+    localStorage.setItem(DICT, "1");
+  }
 };
